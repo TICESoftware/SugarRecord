56db45319b78615aa599e74564576e452d7b27bc
Julian <julian.gierl@gmx.de>
2022-05-30 14:04:37 +0200

    Add Storage implementation that uses NSPersistentContainer

---
+++ SugarRecord/Source/CoreData/Storages/CoreDataHybridStorage.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStorage.swift
+//  iOSCoreData
+//
+//  Created by Julian Gierl on 30.05.22.
+//  Copyright © 2022 in.caramba.SugarRecord. All rights reserved.
+//
+
+import Foundation
